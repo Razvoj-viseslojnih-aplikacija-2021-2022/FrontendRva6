0f84d1211d7ff5c54bd2a915c672f0cc0949bc79
Vladimir Fabri <vfficial98@gmail.com>
2022-05-20 12:00:34 +0200

    Initial frontend commit

---
+++ FrontEndG6/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { VoziloComponent } from './components/primer-components/vozilo/vozilo.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AutomobilComponent } from './components/primer-components/automobil/automobil.component';
+import { ArtiklComponent } from './components/model/artikl/artikl.component';
+import { DobavljacComponent } from './components/model/dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './components/model/porudzbina/porudzbina.component';
+import { StavkaPorudzbineComponent } from './components/model/stavka-porudzbine/stavka-porudzbine.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { HomeComponent } from './components/core/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent,
+    ArtiklComponent,
+    DobavljacComponent,
+    PorudzbinaComponent,
+    StavkaPorudzbineComponent,
+    AboutComponent,
+    AuthorComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
